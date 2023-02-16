6527d984aae0354f46a414eb7664b3a87f837446
JoasChoque <joaschoque@hotmail.com>
2023-02-16 10:45:03 -0300

    Terminando exercicios Matrizes

--- src/vetoresAndMatrizes/Exercicio4.java
+++ src/vetoresAndMatrizes/Exercicio4.java
@@ -8,18 +8,29 @@ public class Exercicio4 {
 		Scanner s = new Scanner(System.in);
 		
 		float notas[][] = new float[10][4];
+		float media[] = new float[10];
+		float soma=0;
 		int linhas,colunas;
 		
+		//Lendo matriz
 		for(linhas=0;linhas<notas.length;linhas++) {
 			for(colunas=0;colunas<notas[colunas].length;colunas++) {
 				System.out.printf("Digite um número para a posição [%d][%d]",linhas,colunas);
 				notas[linhas][colunas]=s.nextFloat();
-			}
 		}
-		
-		
-		
-		
+	}
+			//Criando vetor Média
+		    for(int i=0;i<10;i++) {
+		    	for(int j=0;j<4;j++) {
+					media[i]+=notas[i][j];
+				}
+		    	
+		    	media[i] = media[i]/4;
+		    }
+		    
+		    for(int i=0;i<10;i++) {
+		    	System.out.printf("\nMédia do aluno %d: %.1f\n",(i+1),media[i]);
+		    }
 
 	}
 
